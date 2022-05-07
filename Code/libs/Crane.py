5a63093da60e233925b3691ba5ad31da6a0e9b6a
VitcSS <vitcorreas@gmail.com>
2022-05-06 22:48:30 -0300

    App definitions(INCOMPLETE)

--- Code/libs/Crane.py
+++ Code/libs/Crane.py
@@ -1,4 +1,9 @@
 from libs.object import *
+import tkinter
+import customtkinter
+
+customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 class Crane :
     def __init__(self,
@@ -12,5 +17,21 @@ class Crane :
         self.Tool = Prismatic(Tool,0.06)
         self.Actuator = Magnet(Actuator)
         self.Distance = Ray_Sensor(Distance)
-    
+
+class App(customtkinter.CTk):
+    def __init__(self, Model : Crane):
+        self.Model = Model
+        super().__init__()
+
+        # Joints Position Slider
+        XY_Slider = None
+        Z_Slider = None
+        Tool_Slider = None
+
+        # Magnet Button
+        Magnet_Button = None
+
+        # Status Leds
+        Magnet_Status_Led = None
+
     
--- Code/main.py
+++ Code/main.py
@@ -2,8 +2,4 @@ from libs.Crane import Crane
 from libs.object import object
 from numpy import deg2rad, rad2deg
 Model = Crane()
-print(object.sim.getObjectFloatParam(object.sim.jointfloatparam_upper_limit, Model.XY.handle))
-object.sim.startSimulation()
-Model.XY.setVelocity(5)
-while Model.XY.getPosition() < 90 : pass
-object.sim.stopSimulation()
+
